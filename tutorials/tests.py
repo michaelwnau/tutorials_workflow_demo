57e5a3f6a55bb3e43ca611eec0dc72ecb5980447
Michael Nau <michaelwnau@gmail.com>
2023-02-13 22:42:35 -0600

    first commit

---
+++ tutorials/tests.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.urls import reverse
+from tutorials.models import Tutorial
+import pytest
+# Create your tests here.
+
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+    
+@pytest.fixture
+def new_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.radthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_search_tutorials(new_tutorial):
+    assert Tutorial.objects.filter(title='Pytest').exists()
+    
+def test_update_tutorial(new_tutorial):
+    new_tutorial.title = 'Pytest-Django'
+    new_tutorial.save()
+    assert Tutorial.objects.filter(title='Pytest-Django').exists()
+    
+@pytest.fixture
+def another_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='More-Pytest',
+        tutorial_url='https://pytest-django.radthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_compare_tutorials(new_tutorial, another_tutorial):
+    assert new_tutorial != another_tutorial.pk
